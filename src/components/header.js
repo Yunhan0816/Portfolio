3d80a2591179641a0fc819ad6f2138382be2323f
Hannah Huang <yunhanh@bu.edu>
2020-05-17 18:46:22 -0400

    fix styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,11 @@ import React from "react"
 const Header = () => (
   <div id="header">
     {/* <p> */}
-    <a id="myname" href="/" class="col-12">
+    <a id="myname" href="/">
       {" "}
       Hannah Huang
     </a>{" "}
+    <p>Resume | Github | LinkedIn | Email</p>
     {/* <div class="col-12">||||||||||||||||||||||||||||||||||||||||||||| </div> */}
     {/* </p> */}
   </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,22 +4,22 @@ import DynamicLink from "./dynamic-link"
 import NavLinks from "../data/nav-links.json"
 
 const Nav = () => (
-  <div class="col-12">
-    <nav className="nav">
-      <DynamicLink to="/" className="nav__logo"></DynamicLink>
-      <ul className="nav__list">
-        {NavLinks.map((link, index) => {
-          return (
-            <li key={`nav-item-${index}`} className="nav__item">
-              <DynamicLink className="nav__link" to={link.path}>
-                {link.name}
-              </DynamicLink>
-            </li>
-          )
-        })}
-      </ul>
-    </nav>
-  </div>
+  // <div class="col-12">
+  <nav className="nav">
+    <DynamicLink to="/" className="nav__logo"></DynamicLink>
+    <ul className="nav__list">
+      {NavLinks.map((link, index) => {
+        return (
+          <li key={`nav-item-${index}`} className="nav__item">
+            <DynamicLink className="nav__link" to={link.path}>
+              {link.name}
+            </DynamicLink>
+          </li>
+        )
+      })}
+    </ul>
+  </nav>
+  // </div>
 )
 
 export default Nav
