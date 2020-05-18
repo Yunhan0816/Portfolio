a00ebb3b7bf44d0db5702adea465e72e59144719
Hannah Huang <yunhanh@bu.edu>
2020-05-17 22:52:40 -0400

    add emoji

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,39 @@ import React from "react"
 const Header = () => (
   <div id="header">
     {/* <p> */}
-    <a id="myname" href="/">
-      {" "}
-      Hannah Huang
-      <br></br>
-    </a>{" "}
-    <p style={{ paddingTop: "5px" }}>Resume | Github | LinkedIn | Email</p>
-    {/* <div class="col-12">||||||||||||||||||||||||||||||||||||||||||||| </div> */}
-    {/* </p> */}
+    <div class="row">
+      <a id="myname" href="/">
+        {" "}
+        Hannah Huang
+        <br></br>
+      </a>{" "}
+      <b style={{ fontSize: "35px" }} class="col-2">
+        &#x1f469;&#x1F3FB;&#x200D;&#x1F4BB;
+      </b>
+    </div>
+    <div class="row">
+      <p style={{ paddingTop: "5px" }}>
+        {" "}
+        <a style={{ color: " #c7e6cc" }} href="/">
+          Resume
+        </a>{" "}
+        |{" "}
+        <a style={{ color: " #c7e6cc" }} href="https://github.com/Yunhan0816">
+          Github
+        </a>{" "}
+        |{" "}
+        <a
+          style={{ color: " #c7e6cc" }}
+          href="https://www.linkedin.com/in/hannah-yunhan-huang-007520165/"
+        >
+          LinkedIn
+        </a>{" "}
+        |{" "}
+        <a style={{ color: " #c7e6cc" }} href="mailto:hannahhuang816@gmail.com">
+          Email
+        </a>
+      </p>
+    </div>
   </div>
 )
 
