9e5c135f8be7216431137ec8f7de64b29daac2b9
Hannah Huang <yunhanh@bu.edu>
2020-06-21 13:38:21 -0400

    update mobile view

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -2,16 +2,20 @@ import React from "react"
 import Image from "../components/aboutmepic"
 const AboutMe = () => (
   <div class="col-12">
+    <h1
+      style={{
+        fontSize: "26px",
+        fontWeight: "bold",
+      }}
+    >
+      00. About Me
+    </h1>{" "}
     <div class="row">
-      <div class="col-9">
-        <h1
-          style={{
-            fontSize: "26px",
-            fontWeight: "bold",
-          }}
-        >
-          00. About Me
-        </h1>{" "}
+      <div class="col-12 col-md-3">
+        <Image />
+        <br></br>
+      </div>
+      <div class="col-12 col-md-9">
         <p class="toGrey">
           Hello! I'm Hannah, a 4th year Computer Science student studying at
           Boston University (graduating May 2021). <br></br>
@@ -48,9 +52,6 @@ const AboutMe = () => (
           , an organization that collaborates with non-profits to build
           softwares for social good.
         </p>
-      </div>
-      <div class="col-3">
-        <Image />
       </div>
     </div>
   </div>
